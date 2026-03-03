79c039c9d4982af61f507c37b81bc875afd954fc
nealemanley <49535232+nealemanley@users.noreply.github.com>
2026-03-03 16:51:01 +0000

    Fix ES module syntax in API

--- api/checkout.js
+++ api/checkout.js
@@ -1,6 +1,8 @@
-module.exports = async function handler(req, res) {
+import Stripe from "stripe";
+
+export default async function handler(req, res) {
   return res.status(200).json({ 
     stripe_key_start: (process.env.STRIPE_SECRET_KEY || "MISSING").slice(0, 15),
     node_version: process.version
   });
-};
+}
